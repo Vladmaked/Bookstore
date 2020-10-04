d4e396fd8b83da168ff2588cca5c5226f6b21586
yn22 <yaroslav.naz14@gmail.com>
2020-10-04 16:52:49 +0300

    Removed compiled files from the repo

--- backend/dist/src/api_v1/config/db.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MONGO_URI_CLOUD = exports.MONGO_URI_LOCAL = void 0;
-const { DB_USER = 'admin', DB_PASSWORD = '123super_secret321', DB_NAME = 'bookstore', DB_HOST_LOCAL = 'localhost', DB_PORT_LOCAL = 27017, DB_HOST_CLOUD, } = process.env;
-exports.MONGO_URI_LOCAL = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST_LOCAL}:${DB_PORT_LOCAL}/${DB_NAME}`;
-exports.MONGO_URI_CLOUD = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST_CLOUD}/${DB_NAME}?retryWrites=true&w=majority`;
-//# sourceMappingURL=db.js.map
