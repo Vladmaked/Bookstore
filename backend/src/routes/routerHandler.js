53f662abd129a9f34febe0518ca5c1e445594f58
yn22 <yaroslav.naz14@gmail.com>
2021-01-23 17:57:07 +0200

    fix route handler

--- backend/src/routes/routerHandler.js
+++ backend/src/routes/routerHandler.js
@@ -1,14 +1,18 @@
 const { Router } = require('express');
-const { getUsersRouter, getProductsRouter, getAuthRoute } = require('./');
+const { getUsersRouter, getProductsRouter, getAuthRouter, getCategoriesRouter, getSubcategoriesRouter } = require('./');
 
 const getRoutesHandler = () => {
   const router = Router();
+
+  router.use('/api/auth', getAuthRouter());
 
   router.use('/api/products', getProductsRouter());
 
   router.use('/api/users', getUsersRouter());
 
-  router.use('/api/auth', getAuthRoute());
+  router.use('/api/categories', getCategoriesRouter());
+
+  router.use('/api/subcategories', getSubcategoriesRouter());
 
   return router;
 };
